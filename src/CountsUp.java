be05862f70d370a6dae85608e342639932f2fd98
mlolomo <lombathio356@gmail.com>
2025-07-01 19:02:36 -0400

    Task 1 Completed

---
+++ src/CountsUp.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CountsUp {
+    public static void main(String[] args) {
+        // Task 1
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
